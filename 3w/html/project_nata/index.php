0f1f8435906fd10087c1f10e6d94042211b6a0c2
[alt]legioner9 <legioner9@inbox.ru>
2023-10-10 06:02:37 +0700

    <> ##

--- 3w/html/project_nata/index.php
+++ 3w/html/project_nata/index.php
@@ -4,10 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Мы рады видеть вас на нашем сайте!</title>
-    <link rel="next" href="photos/" > 
-    <?php
-                include '/home/st/REPOBARE/_repo/NWeb/3w/html/project_nata/photos/';
-                ?>
 
     <style>
         /* Стили для главной страницы */
